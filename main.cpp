660c7a9a8446c6b36d16f47850343611688f87b3
this.Destroy() <626562203@qq.com>
2018-08-27 02:11:53 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+﻿#include <QApplication>
+#include "xykeyboardfilter.h"
+#include "xyvirtualkeyboard.h"
+#include "xyskin.h"
+#include <QIcon>
+
+//#define TEST
+#ifdef TEST
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include <QInputMethodEvent>
+
+static void sendInputText(const QString &text)
+{
+    QInputMethodEvent *event = new QInputMethodEvent;
+    event->setCommitString(text);
+    qApp->postEvent(qApp->focusWidget(), event);
+}
+
+#endif
+#include <QDebug>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+//    XYSKIN->writeSkipToFile("I:/DeskTop/test.skin");
+    XYSKIN->loadSkipWithFile(qApp->applicationDirPath() + "/skin/xyinput.skin");
+    XYVirtualKeyboard::getInstance()->show();
+
+#ifdef TEST
+    // 测试代码
+    QWidget widget;
+    QVBoxLayout *layout = new QVBoxLayout(&widget);
+    layout->addWidget(new QLineEdit);
+    layout->addWidget(new QTextEdit);
+    layout->addWidget(new QLineEdit);
+    widget.show();
+    QObject::connect(XYVirtualKeyboard::getInstance(), &XYVirtualKeyboard::send_commit, &sendInputText);
+#endif
+
+    a.setQuitOnLastWindowClosed(true);
+    a.setWindowIcon(QIcon(":/Keyboard.ico"));
+    return a.exec();
+}
